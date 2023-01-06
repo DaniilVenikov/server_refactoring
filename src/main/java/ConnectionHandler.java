0348b4e5b2b3df5d8c831da384816612f772df1a
DaniilVenikov <daniilvenikoff@gmail.com>
2023-01-06 17:48:32 +0300

    Added files in public and Main

--- src/main/java/ConnectionHandler.java
+++ src/main/java/ConnectionHandler.java
@@ -5,12 +5,12 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class ConnectionHandler implements Runnable{
+public class ConnectionHandler implements Runnable {
     final Socket clientSocket;
     final RequestHandlerImpl requestHandler;
     final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
 
-    public ConnectionHandler(Socket clientSocket){
+    public ConnectionHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
         this.requestHandler = new RequestHandlerImpl();
     }
@@ -18,45 +18,47 @@ public class ConnectionHandler implements Runnable{
 
     @Override
     public void run() {
-        while (true) {
-            try (BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream());
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-                final var requestLine = in.readLine();
-                final var parts = requestLine.split(" ");
+            while (true) {
+                try  {
+                    final BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream());
+                    final BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-                if (parts.length != 3) {
-                    // just close socket
-                    continue;
-                }
+                    final var requestLine = in.readLine();
+                    final var parts = requestLine.split(" ");
 
-                final var path = parts[1];
-                if (!requestHandler.isValidPath(out,validPaths,path)) {
-                    continue;
-                }
+                    if (parts.length != 3) {
+                        // just close socket
+                        continue;
+                    }
 
-                final var filePath = Path.of(".", "public", path);
-                final var mimeType = Files.probeContentType(filePath);
+                    final var path = parts[1];
+                    if (!requestHandler.isValidPath(out, validPaths, path)) {
+                        continue;
+                    }
 
-                // special case for classic
-                if (path.equals("/classic.html")) {
-                    final var template = Files.readString(filePath);
-                    final var content = template.replace(
-                            "{time}",
-                            LocalDateTime.now().toString()
-                    ).getBytes();
-                    requestHandler.response(out, mimeType, content.length);
-                    out.write(content);
+                    final var filePath = Path.of(".", "public", path);
+                    final var mimeType = Files.probeContentType(filePath);
+
+                    // special case for classic
+                    if (path.equals("/classic.html")) {
+                        final var template = Files.readString(filePath);
+                        final var content = template.replace(
+                                "{time}",
+                                LocalDateTime.now().toString()
+                        ).getBytes();
+                        requestHandler.response(out, mimeType, content.length);
+                        out.write(content);
+                        out.flush();
+                        continue;
+                    }
+
+                    final var length = Files.size(filePath);
+                    requestHandler.response(out, mimeType, length);
+                    Files.copy(filePath, out);
                     out.flush();
-                    continue;
+                } catch (IOException e){
+                    e.printStackTrace();
                 }
-
-                final var length = Files.size(filePath);
-                requestHandler.response(out,mimeType, length);
-                Files.copy(filePath, out);
-                out.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-        }
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,34 +1,27 @@
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
+    final static int THREADS_QUANTITY = 64;
+    private int port = 9999;
 
-        public void start() {
-            final var executor = Executors.newFixedThreadPool(64);
-            try (final var serverSocket = new ServerSocket(9999)) {
-                while (true) {
-                    try (
-                            final var socket = serverSocket.accept();
-                    ) {
-                        // read only request line for simplicity
-                        // must be in form GET /path HTTP/1.1
-                        executor.execute(new ConnectionHandler(socket));
+    public void start() {
+        final var executor = Executors.newFixedThreadPool(THREADS_QUANTITY);
 
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        try (final var serverSocket = new ServerSocket(port)) {
+            while (true) {
+                final var clientSocket = serverSocket.accept();
+                executor.submit(new ConnectionHandler(clientSocket));
+
             }
-            executor.shutdown();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        executor.shutdown();
+    }
+
+    public void setPort(int newPort){
+        port = newPort;
+    }
 }
